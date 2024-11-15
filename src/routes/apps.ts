0026dac5bf9d54cd312e08756199d1fc989f3683
Nicholas <nicholas.griffin@bbc.co.uk>
2024-11-15 23:10:25 +0000

    chore: refactoring with routes

---
+++ src/routes/apps.ts
@@ -0,0 +1,149 @@
+import { Hono } from 'hono';
+
+import type { IEnv } from '../types';
+import { handlePodcastUpload } from '../services/apps/podcast/upload';
+import { handlePodcastTranscribe, type IPodcastTranscribeBody } from '../services/apps/podcast/transcribe';
+import { handlePodcastSummarise, IPodcastSummariseBody } from '../services/apps/podcast/summarise';
+import { handlePodcastGenerateImage } from '../services/apps/podcast/generate-image';
+
+const app = new Hono();
+
+app.use('/apps/*', async (context, next) => {
+	if (!context.env.ACCESS_TOKEN) {
+		return context.json({
+			response: 'Missing ACCESS_TOKEN binding',
+			status: 400,
+		});
+	}
+
+	const authFromQuery = context.req.query('token');
+	const authFromHeaders = context.req.headers.get('Authorization');
+	const authToken = authFromQuery || authFromHeaders?.split('Bearer ')[1];
+
+	if (authToken !== context.env.ACCESS_TOKEN) {
+		return context.json({
+			response: 'Unauthorized',
+			status: 403,
+		});
+	}
+
+	await next();
+});
+
+app.post('/apps/podcasts/upload', async (context) => {
+	try {
+		const body = await context.req.parseBody();
+
+		const userEmail: string = context.req.headers.get('x-user-email') || '';
+
+		const user = {
+			email: userEmail,
+		};
+
+		const response = await handlePodcastUpload({
+			env: context.env as IEnv,
+			audio: body['audio'] as Blob,
+			user,
+		});
+
+		return context.json({
+			response,
+		});
+	} catch (error) {
+		console.error(error);
+
+		return context.json({
+			response: 'Something went wrong, we are working on it',
+		});
+	}
+});
+
+app.post('/apps/podcasts/transcribe', async (context) => {
+	try {
+		const body = (await context.req.json()) as IPodcastTranscribeBody;
+
+		const userEmail: string = context.req.headers.get('x-user-email') || '';
+
+		const user = {
+			email: userEmail,
+		};
+
+		const newUrl = new URL(context.req.url);
+		const appUrl = `${newUrl.protocol}//${newUrl.hostname}`;
+
+		const response = await handlePodcastTranscribe({
+			env: context.env as IEnv,
+			request: body,
+			user,
+			appUrl,
+		});
+
+		return context.json({
+			response,
+		});
+	} catch (error) {
+		console.error(error);
+
+		return context.json({
+			response: 'Something went wrong, we are working on it',
+		});
+	}
+});
+
+app.post('/apps/podcasts/summarise', async (context) => {
+	try {
+		const body = (await context.req.json()) as IPodcastSummariseBody;
+
+		const userEmail: string = context.req.headers.get('x-user-email') || '';
+
+		const user = {
+			email: userEmail,
+		};
+
+		const response = await handlePodcastSummarise({
+			env: context.env as IEnv,
+			request: body,
+			user,
+		});
+
+		return context.json({
+			response,
+		});
+	} catch (error) {
+		console.error(error);
+
+		return context.json({
+			response: 'Something went wrong, we are working on it',
+		});
+	}
+});
+
+app.post('/apps/podcasts/generate-image', async (context) => {
+	try {
+		const body = (await context.req.json()) as IPodcastTranscribeBody;
+
+		const userEmail: string = context.req.headers.get('x-user-email') || '';
+
+		const user = {
+			email: userEmail,
+		};
+
+		const response = await handlePodcastGenerateImage({
+			env: context.env as IEnv,
+			request: body,
+			user,
+		});
+
+		return context.json({
+			response,
+		});
+	} catch (error) {
+		console.error(error);
+
+		return context.json({
+			response: 'Something went wrong, we are working on it',
+		});
+	}
+});
+
+export default app;
---
+++ src/routes/webhooks.ts
@@ -0,0 +1,52 @@
+import { Hono } from 'hono';
+
+import type { IEnv, IBody } from '../types';
+import { handleReplicateWebhook } from '../services/webhooks/replicate';
+
+const app = new Hono();
+
+app.use('/webhooks/*', async (context, next) => {
+	if (!context.env.WEBHOOK_SECRET) {
+		return context.json({
+			response: 'Missing WEBHOOK_SECRET binding',
+			status: 400,
+		});
+	}
+
+	const tokenFromQuery = context.req.query('token');
+
+	if (tokenFromQuery !== context.env.WEBHOOK_SECRET) {
+		return context.json({
+			response: 'Unauthorized',
+			status: 403,
+		});
+	}
+
+	await next();
+});
+
+app.post('/webhooks/replicate', async (context) => {
+	try {
+		const body = (await context.req.json()) as IBody;
+
+		const id = context.req.query('chatId');
+
+		const data = await handleReplicateWebhook(
+			{
+				env: context.env as IEnv,
+				request: body,
+			},
+			id
+		);
+
+		return context.json(data);
+	} catch (error) {
+		console.error(error);
+
+		return context.json({
+			response: 'Something went wrong, we are working on it',
+		});
+	}
+});
+
+export default app;
