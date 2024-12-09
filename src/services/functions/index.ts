207987797318fc4dd1db20c21048044d180f3280
Nicholas <nicholas.griffin@bbc.co.uk>
2024-12-09 18:56:11 +0000

    chore: note tools because AI is using it too often

--- src/services/functions/index.ts
+++ src/services/functions/index.ts
@@ -1,15 +1,11 @@
 import type { IFunction, IFunctionResponse, IRequest } from "../../types";
 import { AppError } from "../../utils/errors";
-import { create_note } from "./create_note";
-import { get_note } from "./get_note";
 import { create_image } from "./image";
 import { create_music } from "./music";
 import { create_video } from "./video";
 import { get_weather } from "./weather";
 
 export const availableFunctions: IFunction[] = [
-	create_note,
-	get_note,
 	get_weather,
 	create_video,
 	create_music,
